0128ad5e712486ad7067fae950990f7496c3bbdd
Don Reniff <donaldreniff@gmail.com>
2023-03-29 12:00:07 -0700

    add state and pages

--- exercise-frontend-ui/src/App.js
+++ exercise-frontend-ui/src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+// Import dependencies
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { useState } from 'react';
+import { GiLog } from 'react-icons/gi';
+
+// Import Pages
+import HomePage from './pages/HomePage';
+import AddExercisePage from './pages/AddExercisePage';
+import EditExercisePage from './pages/EditExercisePage';
+
 import './App.css';
 
+// Define the function that renders the content in routes using State
 function App() {
+
+  const [exercise, setExercise] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+
+        <header>
+          <div id="header">
+            <h1><span id="header-1">Workout</span> <GiLog id="logo" /> <span id="header-2">Log</span></h1>
+            <p>A place to record your exercises.</p>
+          </div>
+
+        </header>
+
+        <main>
+          <Route path="/" exact>
+            <HomePage setExercise={setExercise} />
+          </Route>
+
+          <Route>
+            <AddExercisePage />
+          </Route>
+
+          <Route>
+            <EditExercisePage exercise={exercise} />
+          </Route>
+        </main>
+
+        <footer>
+          <p><cite>&copy; 2022 Don Reniff</cite></p>
+        </footer>
+
+      </Router>
+    </>
   );
 }
 
