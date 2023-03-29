e9198444da22183daad95d08ab98b2cbff9fe8ad
Don Reniff <donaldreniff@gmail.com>
2023-03-29 00:28:06 -0700

    add code for connecting to MongoDB

---
+++ exercise-backend-rest/exercise-model.mjs
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+import 'doteven/config';
+
+mongoose.connect(
+    process.env.MONGODB_CONNECT_STRING,
+    { useNewUrlParser: true }
+);
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+    console.log("Successfully connected to MongoDB using Mongoose!");
+});
+
