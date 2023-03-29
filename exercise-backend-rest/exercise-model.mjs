39b68d3a41a42d1c1e6719dbb40097e8fc5845d2
Don Reniff <donaldreniff@gmail.com>
2023-03-29 00:48:59 -0700

    add variable for updating documents

--- exercise-backend-rest/exercise-model.mjs
+++ exercise-backend-rest/exercise-model.mjs
@@ -48,3 +48,15 @@ const findExerciseById = async (_id) => {
     const query = Exercise.findById(_id);
     return query.exec();
 }
+
+// REPLACE model *****************************************************
+const replaceExercise = async (_id, name, reps, weight, unit, date) => {
+    const result = await Exercise.replaceOne({_id: _id }, {
+        name: name,
+        reps: reps,
+        weight: weight,
+        unit: unit,
+        date: date
+    });
+    return result.modifiedCount;
+}
