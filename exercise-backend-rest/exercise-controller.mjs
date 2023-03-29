1e52ba4058c7b0de2fa4520d0763061e69a0f45e
Don Reniff <donaldreniff@gmail.com>
2023-03-29 00:32:31 -0700

    add route handler for POST requests

---
+++ exercise-backend-rest/exercise-controller.mjs
@@ -0,0 +1,31 @@
+import 'dotenv/config';
+import express from 'express';
+import * as exercises from './exercise-model.mjs';
+
+const PORT = process.env.PORT;
+const app = express();
+app.use(express.json());
+
+
+// CREATE controller ******************************************
+app.post ('/exercises', (req,res) => { 
+    exercises.createExercise(
+        req.body.name, 
+        req.body.reps, 
+        req.body.weight,
+        req.body.unit,
+        req.body.date
+        )
+        .then(exercise => {
+            res.status(201).json(exercise);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(400).json({ error: 'Invalid request.' });
+        });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
