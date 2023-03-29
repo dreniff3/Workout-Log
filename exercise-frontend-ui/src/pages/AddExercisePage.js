4e363b4ab49bd10069753b72dd044800628603ae
Don Reniff <donaldreniff@gmail.com>
2023-03-29 14:08:53 -0700

    add export declaration

--- exercise-frontend-ui/src/pages/AddExercisePage.js
+++ exercise-frontend-ui/src/pages/AddExercisePage.js
@@ -89,3 +89,5 @@ export const AddExercisePage = () => {
         </>
     );
 }
+
+export default AddExercisePage;
--- exercise-frontend-ui/src/pages/EditExercisePage.js
+++ exercise-frontend-ui/src/pages/EditExercisePage.js
@@ -92,3 +92,5 @@ export const EditExercisePage = ({ exercise }) => {
         </>
     );
 }
+
+export default EditExercisePage;
