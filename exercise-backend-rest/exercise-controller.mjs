228467649c5ff4b13cdd063798a3c28d1eb878fb
Don Reniff <donaldreniff@gmail.com>
2023-03-29 00:56:41 -0700

    add route handler for Delete requests

--- exercise-backend-rest/exercise-controller.mjs
+++ exercise-backend-rest/exercise-controller.mjs
@@ -101,6 +101,22 @@ app.put('/exercises/:_id', (req, res) => {
     });
 });
 
+// DELETE Controller ******************************
+app.delete('/exercises/:_id', (req, res) => {
+    exercises.deleteById(req.params._id)
+        .then(deletedCount => {
+            if (deletedCount === 1) {
+                res.status(204).send();
+            } else {
+                res.status(404).json({ Error: 'Document not found' });
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            res.send({ error: 'Request to delete a document failed' });
+        });
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`);
