ff3503e726272693e98771c99e815d329cf495bb
Don Reniff <donaldreniff@gmail.com>
2023-03-29 00:50:03 -0700

    add route handler for PUT requests

--- exercise-backend-rest/exercise-controller.mjs
+++ exercise-backend-rest/exercise-controller.mjs
@@ -70,6 +70,37 @@ app.get('/exercises', (req, res) => {
 
 });
 
+// UPDATE controller ************************************
+app.put('/exercises/:_id', (req, res) => {
+    exercises.replaceExercise(
+        req.params._id, 
+        req.body.name, 
+        req.body.reps, 
+        req.body.weight,
+        req.body.unit,
+        req.body.date
+    )
+
+    .then(numUpdated => {
+        if (numUpdated === 1) {
+            res.status(200).json({ 
+                _id: req.params._id, 
+                name: req.body.name, 
+                reps: req.body.reps, 
+                weight: req.body.weight,
+                unit: req.body.unit,
+                date: req.body.date
+            })
+        } else {
+            res.status(404).json({ Error: 'Document not found' });
+        }
+    })
+    .catch(error => {
+        console.error(error);
+        res.status(400).json({ Error: 'Invalid request.' });
+    });
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`);
